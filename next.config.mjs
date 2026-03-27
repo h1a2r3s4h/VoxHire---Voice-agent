3611bef8ebe0f20f5deb22bfadf97489703c769d
h1a2r3s4h <harshitgangwar1601@gmail.com>
2026-03-27 12:03:46 +0530

    refactor: streamline webpack alias configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,13 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+      encoding: false,
+    };
     return config;
   },
 };
+
 export default nextConfig;
